4e1fe838a956b56f399247ef0529aee3df17f578
mohatamimhaque <72484860+mohatamimhaque@users.noreply.github.com>
2021-08-10 23:03:28 +0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,9 @@
+var menuIcon = document.querySelector(".menu-icon2");
+var sidebar = document.querySelector(".");
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("sidebar-third")
+}
+
+
+
